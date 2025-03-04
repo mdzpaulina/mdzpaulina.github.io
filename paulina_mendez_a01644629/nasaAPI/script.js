7d36bdea65cbafeb36ee231a85a3fcb2178be2ab
mdzpau <mdzlopezp@gmail.com>
2025-03-04 17:53:22 -0600

    added nasa api

---
+++ paulina_mendez_a01644629/nasaAPI/script.js
@@ -0,0 +1,17 @@
+async function obtenerCielo() {
+    const apiKey = "GTJjKvFA7ZD6oyeGshaMJMtmDTq7pSE9yvsloZlo"; 
+    const fecha = document.getElementById("fechaNacimiento").value;
+    if (!fecha) {
+        alert("Por favor, ingresa una fecha válida.");
+        return;
+    }
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${fecha}`;
+    
+    let respuesta = await fetch(url);
+    let data = await respuesta.json();
+    console.log(data);
+    document.getElementById("resultado").innerHTML = 
+        `<h2>${data.title} (${data.date})</h2>
+        <img src="${data.url}" alt="Imagen del espacio" width="600">
+        <p>${data.explanation}</p>`;
+}
