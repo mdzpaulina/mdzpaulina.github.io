adb72ba9f9808c426a3bb0a42f5f9ccbefeacf28
mdzpau <mdzlopezp@gmail.com>
2025-03-04 16:38:59 -0600

    changes

---
+++ ejercicios/src/js/jueves27.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.carousel');
+    const instances = M.Carousel.init(elems, {
+      // specify options here
+    });
+  });
+
+
+const instance = M.Carousel.init({
+    fullWidth: true
+  });
+      
